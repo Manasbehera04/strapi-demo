2d0849354c70b687e20634731028716955a61aea
Manas Ranjan <mbehera04@gmail.com>
2022-08-29 17:58:49 +0530

    Installed pg, aws-S3 upload provider and updated the config files
    Installed pg, aws-S3 upload provider and updated the config files

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env("DATABASE_HOST", "database-2.cnvzf9gkbojw.us-east-1.rds.amazonaws.com"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "postgres"),
       user: env("DATABASE_USERNAME", "postgres"),
